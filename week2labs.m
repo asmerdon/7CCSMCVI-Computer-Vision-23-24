61fdfe0c92cf237d97c73c00587f8b8ee366ab50
Alexander Smerdon <43986628+asmerdon@users.noreply.github.com>
2023-10-10 13:41:18 +0100

    Add files via upload

--- week2labs.m
+++ week2labs.m
@@ -70,5 +70,46 @@ Ibdiff(498, 400)
 bw(498,400)
 
 %Excercise 3:
-Ibcorr=corr2(Ib, Ibdiffv)
-figure(7), imagesc(Icorr)
+offsets=[0:30];
+for offset=offsets
+   simb(offset+1)=corr2(Ib(1:end-offset,:),Ib(1+offset:end,:));
+   simc(offset+1)=corr2(Ic(1:end-offset,:),Ic(1+offset:end,:));
+end
+figure(8), clf,
+plot(offsets,simb);
+hold on;
+plot(offsets,simc);
+legend({'elephant','woods'});
+xlabel('shift'); ylabel('correlation coefficient')
+
+%To simulate a retinal ganglion cell we use a Difference of Gaussians (DoG)
+%mask (or operator):
+%To produce a DoG mask we can use the MATLAB command fspecial to create two
+%Gaussian masks that are subtracted from each other:
+dog=fspecial('gaussian',11,1.25)-fspecial('gaussian',11,1.75);
+%The binary image and the mask are convolved together:
+Ibdog=conv2(Ibd,dog,'same');
+%Plotted:
+figure, clf, imagesc(Ibdog); colormap('gray'); colorbar
+%Excercise 4:
+Ibdog(170,493)
+
+%In the previous section, we created the DoG (by subtracting one Gaussian
+%from another) and then convolved this with the image. It would have been possible to produce an identical result by convolving the image twice with two different Gaussian masks, and taking the difference between the two outputs of these convolutions.%
+%This approach is used to simulate colour opponent cells, except each
+%Gaussian is convolved with a different colour channel of a colour image:
+Iad=im2double(Ia);
+%First create two Gaussians to simulate the centre and surround of the cell's RF:
+gc=fspecial('gaussian',9,1);
+gs=fspecial('gaussian',9,1.5);
+%You can then simulate the response of a red-on, green-off colour opponent cell by executing:
+IaRG=conv2(Iad(:,:,1),gc,'same')-conv2(Iad(:,:,2),gs,'same');
+%Note colour channels are the third-dimension of Iad and channel 1 is the
+%red channel, and channel 2 is the green channel.
+%Excercise 5:
+%Green on, red off:
+IaGR=conv2(Iad(:,:,2),gc,'same')-conv2(Iad(:,:,1),gs,'same');
+%Blue on, yellow off:
+IaBY=conv2(Iad(:,:,3),gc,'same')-conv2(Iad(:,:,2),gs,'same');
+%Yellow on, blue off:
+IaYB=conv2(Iad(:,:,1),gc,'same')-conv2(Iad(:,:,2),gs,'same');
